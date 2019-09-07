3b773d69f5449fae5de281daf38b25e846243ec3
vedangparasnis <vedang.parasnis921@gmail.com>
2019-09-08 00:51:32 +0530

    changes

--- lib/Data.dart
+++ lib/Data.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'dart:async';
+import 'package:flutter_app/Sample.dart';
 import 'dart:math';
 
 class Data extends StatefulWidget {
@@ -20,30 +21,78 @@ class Home extends State<Data> {
   @override
   Widget build(BuildContext context) {
     return new Material(
-      child: new Column(
+      child: Column(
         children: <Widget>[
-          Container(
-              child: AppBar(
-            title: new DecoratedBox(
-              decoration: new BoxDecoration(
-                  color: Colors.transparent,
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.topRight,
-                      stops: [
-                        0.7,
-                        0.8,
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height * 0.33,
+                width: MediaQuery.of(context).size.width,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          stops: [
+                        0.1,
                         0.5,
-                        1
+                        0.7,
+                        0.9
                       ],
-                      colors: [
+                          colors: [
                         Colors.red[600],
                         Colors.red[700],
                         Colors.red[800],
-                        Colors.red[900]
+                        Colors.red[900],
                       ])),
+                ),
+              ),
+              AppBar(
+                  backgroundColor: Colors.transparent,
+                  title: Text(
+                    "The data Page",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        stops: [
+                      0.1,
+                      0.2,
+                      0.8,
+                      0.91
+                    ],
+                        colors: [
+                      Colors.greenAccent[500],
+                      Colors.greenAccent[600],
+                      Colors.greenAccent[700],
+                      Colors.greenAccent[800],
+                    ])),
+                child: Text(
+                  "BACK",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "cursive",
+                      decoration: TextDecoration.none),
+                ),
+              ),
+              onPressed: () {
+                print("pressed now");
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) => new Sample()));
+              },
             ),
-          ))
+          )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter_app/Sample.dart';
 import 'package:vector_math/vector_math_lists.dart';
 import 'dart:math';
 import 'dart:async';
+import 'package:flutter_app/Data.dart';
 //firebase stuff
 
-void main()=>runApp(UserApp());
+void main() => runApp(UserApp());
 
-class UserApp extends StatelessWidget{
-
-  Widget build(BuildContext context){
+class UserApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
@@ -20,79 +20,126 @@ class UserApp extends StatelessWidget{
   }
 }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     print("the widget is instantiated");
-      return MaterialApp(
-        title: "te pixel 3 app",
-        home: Scaffold(
-          drawer: Drawer(
-            child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(color: Colors.blue),
-                    child: Icon(Icons.accessible),
-                  ),
-                ),
-                ListTile(
-                  title: Text("the user app",
-                  style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.end,),
-                  onLongPress: (){
-                    Navigator.push(context, new MaterialPageRoute(builder: (context)=>new Sample()));
-                  },
-                )
-              ],
-            ),
-          ),
-          appBar: AppBar(
-            title: Text("the android sdk",textAlign: TextAlign.end,style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black.withOpacity(0.91),
-              decoration: TextDecoration.none,
-              decorationStyle: TextDecorationStyle.solid,
-              decorationColor: Colors.blue
-            ),),
-                backgroundColor: Colors.red,
-                bottom: TabBar(
-                  tabs: <Widget>[
-                    Tab(
-                      child: Icon(Icons.edit),
-                    ),
-                    Tab(
-                      child: Icon(Icons.enhanced_encryption),
-                    )
-                  ],
-                ),
-          ),
-          body: TabBarView(
+    return MaterialApp(
+      title: "te pixel 3 app",
+      home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
             children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Center(
-                    child: Icon(Icons.add_call),
-                  )
-                ],
+              DrawerHeader(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.blue),
+                  child: Icon(Icons.accessible),
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              ListTile(
+                title: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.greenAccent),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "the user app",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            fontStyle: FontStyle.italic),
+                        textAlign: TextAlign.end,
+                      ),
+                    )),
+                onLongPress: () {
+                  Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new Sample()));
+                },
+              ),
+              ListBody(
                 children: <Widget>[
-                  Container(
-
-                    child: Icon(Icons.call_end),
-                  )
+                  ListTile(
+                      onTap: () {
+                        print("in tap mode");
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) => new Data()));
+                      },
+                      title: DecoratedBox(
+                          decoration: BoxDecoration(color: Colors.black),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "The Data Page",
+                              style: TextStyle(
+                                  color: Colors.orangeAccent,
+                                  fontFamily: "cursive",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.italic,
+                                  decoration: TextDecoration.none),
+                            ),
+                          )))
                 ],
               ),
             ],
           ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.next_week),
-              onPressed: (){
-                  Navigator.push(context, new MaterialPageRoute(builder: (context)=> new Sample()));
-              },
+        ),
+        appBar: AppBar(
+          title: Text(
+            "the android sdk",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black.withOpacity(0.91),
+                decoration: TextDecoration.none,
+                decorationStyle: TextDecorationStyle.solid,
+                decorationColor: Colors.blue),
           ),
+          backgroundColor: Colors.red,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Icon(Icons.edit),
+              ),
+              Tab(
+                child: Icon(Icons.enhanced_encryption),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Icon(Icons.add_call),
+                )
+              ],
             ),
-          );
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 25),
+                  child: Icon(Icons.call_end),
+                )
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.next_week),
+          onPressed: () {
+            Navigator.push(context,
+                new MaterialPageRoute(builder: (context) => new Sample()));
+          },
+        ),
+      ),
+    );
   }
 }
